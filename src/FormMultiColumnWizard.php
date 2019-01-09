43706a7b2f26d59059a59ef0038cf90c5f38518b
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-01-09 11:56:25 +0100

    Add updateMcwInputs function.

--- src/FormMultiColumnWizard.php
+++ src/FormMultiColumnWizard.php
@@ -68,24 +68,49 @@ class FormMultiColumnWizard extends \MenAtWork\MultiColumnWizardBundle\Contao\Wi
             var clonedRow = $(this).closest('tr').clone();
             clonedRow.find('input').val('');
             $(this).closest('tr').after(clonedRow);
+
+            updateMcwInputs($(this).closest('table'));
         });
 
         $(document).on('click', 'table.multicolumnwizard a[data-operations="up"]', function (event) {
             event.preventDefault();
             var row = $(this).parents('tr:first');
             row.insertBefore(row.prev());
+
+            updateMcwInputs($(this).closest('table'));
         });
 
         $(document).on('click', 'table.multicolumnwizard a[data-operations="down"]', function (event) {
             event.preventDefault();
             var row = $(this).parents('tr:first');
             row.insertAfter(row.prev());
+
+            updateMcwInputs($(this).closest('table'));
         });
 
         $(document).on('click', 'table.multicolumnwizard a[data-operations="delete"]', function (event) {
             event.preventDefault();
             $(this).closest('tr').remove();
+
+            updateMcwInputs($(this).closest('table'));
         });
+
+        function updateMcwInputs($table) {
+            $table.find('tr[data-rowid]').each(function (index) {
+                $(this).attr('data-rowid', index);
+
+                $(this).find('label[for]').each(function () {
+                    $(this).attr('for', $(this).attr('for').replace(/(.+?)(\[\d])(\[.+?])/, '$1[' + index + ']$3'))
+                });
+
+                $(this).find('*[name][id]').each(function () {
+                    var name = $(this).attr('name').replace(/(.+?)(\[\d])(\[.+?])/, '$1[' + index + ']$3');
+                    $(this).attr('name', name);
+                    $(this).attr('id', 'ctrl_' + name);
+                });
+            });
+        }
+
     }(jQuery));
 </script>
 HTML;
@@ -101,9 +126,16 @@ HTML;
         $arrItems,
         $arrHiddenHeader = array(),
         $onlyRows = false
-    )
-    {
-        $return = parent::generateTable($arrUnique, $arrDatepicker, $arrColorpicker, $strHidden, $arrItems, $arrHiddenHeader, $onlyRows);
+    ) {
+        $return = parent::generateTable(
+            $arrUnique,
+            $arrDatepicker,
+            $arrColorpicker,
+            $strHidden,
+            $arrItems,
+            $arrHiddenHeader,
+            $onlyRows
+        );
 
         // fixme does not work
         $return = preg_replace('/<script(.*?)>(.*?)<\/script>/im', '', $return);
@@ -138,8 +170,8 @@ HTML;
                         http_build_query(
                             [
                                 $this->strCommand => $button,
-                                'cid' => $level,
-                                'id' => $this->currentRecord,
+                                'cid'             => $level,
+                                'id'              => $this->currentRecord,
                             ]
                         ),
                         false
